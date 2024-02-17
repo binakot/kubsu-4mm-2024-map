556423e92c6d9afcaeb3e9157cbfc10a68ddefff
Ivan Muratov <binakot@gmail.com>
2024-02-17 10:56:08 +0300

    html css js

--- web/script.js
+++ web/script.js
@@ -1,3 +1,12 @@
 function hello() {
     return "Hello, World"
 }
+
+const matches = document.querySelectorAll("p");
+console.log(matches)
+
+let size = 1;
+setInterval(() => {
+    size += 1
+    matches[0].style.fontSize = size + 'px';
+}, 100)
